c9dd51f6579bd2f42a6f3f620f55fb6cdafec58d
joycech333 <127618148+joycech333@users.noreply.github.com>
2024-03-11 14:14:32 -0700

    Inference with token-by-token input and cache eviction
    Integrate run_streaming_llama.py with functionality from eval_long_ppl.py. Intended to address the following Github issue from StreamingLLM:
    
    "As illustrated in our run_streaming_llama.py, the KV cache eviction occurs only before the prompt input and generation. This means the demo code isn't designed for single, long input samples.
    
    However, for long text inputs with LLMs, you can reference our perplexity evaluation script. Here, we input text and evict the KV cache token-by-token."

---
+++ run_tokenwise.py
@@ -0,0 +1,153 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import torch
+import argparse
+import json
+import os
+import time
+import re
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+os.environ["TRANSFORMERS_CACHE"] = "/deep/group/aicc-bootcamp/vicuna"
+
+from tqdm import tqdm
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from peft import PeftConfig, PeftModel
+from streaming_llm.utils import load, download_url, load_jsonl
+from streaming_llm.enable_streaming_llm import enable_streaming_llm
+
+@torch.no_grad()
+def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len, kv_cache=None):
+    outputs = model(
+        input_ids=input_ids,
+        past_key_values=past_key_values,
+        use_cache=True
+    )
+    past_key_values = outputs.past_key_values
+    pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
+    generated_ids = [pred_token_idx.item()]
+    pos = 0
+
+    for _ in range(max_gen_len - 1):
+        if kv_cache is not None:
+            past_key_values = kv_cache(past_key_values)  # Evict tokens before generating the next token
+
+        outputs = model(
+            input_ids=pred_token_idx,
+            past_key_values=past_key_values,
+            use_cache=True,
+        )
+        past_key_values = outputs.past_key_values
+        pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
+        generated_ids.append(pred_token_idx.item())
+
+        generated_text = (
+            tokenizer.decode(
+                generated_ids,
+                skip_special_tokens=True,
+                clean_up_tokenization_spaces=True,
+                spaces_between_special_tokens=False,
+            )
+            .strip()
+            .split(" ")
+        )
+
+        now = len(generated_text) - 1
+        if now > pos:
+            print(" ".join(generated_text[pos:now]), end=" ", flush=True)
+            pos = now
+
+        if pred_token_idx == tokenizer.eos_token_id:
+            break
+    print(" ".join(generated_text[pos:]), flush=True)
+
+    return past_key_values
+
+@torch.no_grad()
+def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=2500):
+    past_key_values = None
+    for idx, prompt in enumerate(prompts):
+        prompt = "USER: " + prompt + "\n\nASSISTANT: "
+        print("\n" + prompt, end="")
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+        input_ids = input_ids.to(model.device)
+        seq_len = input_ids.shape[1]
+        if kv_cache is not None:
+            space_needed = seq_len + max_gen_len
+            # Evict tokens before processing each token in the input sequence
+            for _ in range(seq_len):
+                past_key_values = kv_cache(past_key_values)
+
+        past_key_values = greedy_generate(
+            model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len, kv_cache=kv_cache
+        )
+
+def main(args):
+    # PeftConfig from peft model
+    base_model_path = args.base_model_path
+    config = PeftConfig.from_pretrained(args.peft_model_path)
+    
+    # Quantization config
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+    )
+
+    # Load base model
+    model = AutoModelForCausalLM.from_pretrained(
+        config.base_model_name_or_path,
+        device_map="auto",
+        torch_dtype=torch.float16,
+        trust_remote_code=True,
+        quantization_config=bnb_config,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        config.base_model_name_or_path,
+        trust_remote_code=True,
+    )
+
+    model = PeftModel.from_pretrained(model, args.peft_model_path)
+
+    test_filepath = "/deep/u/joycech/LLaVA/data/example_test_instructions.jsonl"
+    print(f"Loading data from {test_filepath} ...")
+
+    list_data = load_jsonl(test_filepath)
+    prompts = []
+    for sample in list_data:
+        prompts += sample["turns"]
+
+    if args.enable_streaming:
+        kv_cache = enable_streaming_llm(
+            model, start_size=args.start_size, recent_size=args.recent_size
+        )
+    else:
+        kv_cache = None
+
+    streaming_inference(
+        model,
+        tokenizer,
+        prompts,
+        kv_cache,
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--base_model_path", type=str, default="lmsys/vicuna-7b-v1.5"
+    )
+    parser.add_argument(
+        "--peft_model_path", type=str, default="/deep/u/joycech/LLaVA/checkpoints/full-data-finetuned-vicuna"
+    )
+    parser.add_argument("--data_root", type=str, default="data/")
+    parser.add_argument("--enable_streaming", action="store_true")
+    # parser.add_argument("--enable_streaming", action="store_true")
+    parser.add_argument("--start_size", type=int, default=4)
+    parser.add_argument("--recent_size", type=int, default=4000)
+    args = parser.parse_args() 
+
+    main(args)
